044cf8d78354f3f4ee132894cd959832da4163e1
vishipayyallore <vishipayyallore@yahoo.com>
2015-09-22 09:04:54 +0530

    AAA

---
+++ src/copwebapplication/Services/Mailer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using SendGrid;
+
+namespace copwebapplication.Services
+{
+    public class Mailer
+    {
+        private static string from;
+        private static string sub;
+        private static string bodyHtml;
+        private static string username;
+        private static string password;
+        
+        static Mailer()
+        {
+            from = "reachSajad@gmail.com";
+            sub = "Cloud Sample Mail!";
+            bodyHtml = "<p>{0}</p>";
+            username = "azure_dce1d67beb5656279cff418febdda2f6@azure.com";
+            password = "logjvYk2w6vMQb1";
+        }
+
+        public static bool SendEmail(string to, string body) 
+        {
+            SendGridMessage email = new SendGridMessage() 
+            {
+                From = new MailAddress(from),
+                Subject = sub,
+                Html = String.Format(bodyHtml, body)
+            };
+
+            bool retVal = true;
+
+            try
+            {
+                email.AddTo(to.Split(new char[] { ',' }));
+                SendGrid.Web webTransport = new SendGrid.Web(new NetworkCredential(username, password));
+                webTransport.DeliverAsync(email);
+            }
+            catch
+            {
+                retVal = false;
+            }
+
+            return retVal;
+        }
+    }
+}
---
+++ src/copwebapplication/Services/Run.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading;
+
+namespace copwebapplication.Services
+{
+    public static class Run
+    {
+        private const int Second = 1000;
+
+        public static void TightLoop(int retries, Action routine)
+        {
+            int retryCount = retries;
+            int currentCount = 0;
+
+            do
+            {
+                try
+                {
+                    routine();
+                    break;
+                }
+                catch 
+                {
+                    if (++currentCount >= retryCount) break;
+                }
+            } while (true);
+        }
+
+        public static void WithDefault(int retries, int interval, Action routine)
+        {
+            int retryCount = retries;
+            int backOffInterval = interval;
+            int currentCount = 0;
+
+            do
+            {
+                try
+                {
+                    routine();
+                    break;
+                }
+                catch 
+                {
+                    if (++currentCount >= retryCount) break;
+                    Thread.Sleep(backOffInterval * Second);
+                }
+            } while (true);
+        }
+
+        public static void WithRandomInterval(int retries, int intervalMin, int intervalMax, Action routine)
+        {
+            Random randomGen = new Random();
+            int retryCount = retries;
+            int minInterval = intervalMin;
+            int maxInterval = intervalMax;
+            int backOffInterval = 0;
+            int currentCount = 0;
+            
+            do
+            {
+                try
+                {
+                    routine();
+                    break;
+                }
+                catch 
+                {
+                    if (++currentCount >= retryCount) break;
+                    backOffInterval = randomGen.Next(minInterval, maxInterval);
+                    Thread.Sleep(backOffInterval * Second);
+                }
+            } while (true);
+        }
+
+        public static void WithProgressBackOff(int retries, int intervalMin, int intervalMax, Action routine)
+        {
+            int retryCount = retries;
+            int minInterval = intervalMin;
+            int maxInterval = intervalMax;
+            int backOffInterval = intervalMin;
+            int exponent = 2;
+            int currentCount = 0;
+
+            do
+            {
+                try
+                {
+                    routine();
+                    break;
+                }
+                catch 
+                {
+                    if (++currentCount >= retryCount) break;
+                    Thread.Sleep(backOffInterval * Second);
+                    backOffInterval = Math.Min(maxInterval, backOffInterval * exponent);
+                }
+            } while (true);
+
+        }
+    }
+}
